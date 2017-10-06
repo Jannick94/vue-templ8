18ea67145787254de4cce0e334f3c584e0cea2c4
Jannick Berkhout <jannick@moodles.nl>
2017-10-07 00:33:19 +0200

    Add basic button dropdown component

---
+++ src/components/core/elements/Button/AppDropdownButton.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="app-dropdown-button">
+        <div class="dropdown-button">
+            <AppButton class="primary dropdown-button-primary" @click.native="primary()">Dropdown</AppButton>
+            <AppButton class="primary dropdown-button-secondary" @click.native="toggle()">
+                <i class="material-icons">arrow_drop_down</i>
+            </AppButton>
+        </div>
+        <ul class="dropdown-button-list" v-show="isOpen">
+            <li class="dropdown-button-list-item">Item</li>
+            <li class="dropdown-button-list-item">Item</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import AppButton from '@/components/core/elements/Button/AppButton';
+
+    export default {
+        name: 'AppDropdownButton',
+        components: {
+            AppButton
+        },
+        data() {
+            return {
+                isOpen: false
+            }
+        },
+        methods: {
+            primary() {
+                this.$emit('primary');
+            },
+            toggle() {
+                this.isOpen = !this.isOpen;
+                this.$emit('toggle', this.isOpen);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .app-dropdown-button {
+        display: inline-block;
+
+        .dropdown-button {
+            display: flex;
+
+            .button {
+                margin: 0;
+                padding: 8px 20px;
+            }
+
+            .dropdown-button-primary {
+                border-radius: 2px 0px 0px 2px;
+                border-right: none;
+            }
+
+            .dropdown-button-secondary {
+                border-radius: 0px 2px 2px 0px;
+                padding: 10px;
+            }
+        }
+
+        .dropdown-button-list {
+            display: flex;
+            flex-direction: column;
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            background: #f9f9f9;
+            border: 1px solid #ddd;
+            border-radius: 2px;
+            padding: 5px 0;
+
+            .dropdown-button-list-item {
+                padding: 15px 10px;
+
+                &:hover {
+                    background: #ddd;
+                }
+            }
+        }
+    }
+</style>
--- src/components/pages/Buttons.vue
+++ src/components/pages/Buttons.vue
@@ -5,19 +5,21 @@
 
         <h2>Button Outline</h2>
         <AppButton class="primary outline">Button</AppButton>
+
+        <h2>Button dropdown</h2>
+        <AppDropdownButton></AppDropdownButton>
     </div>
 </template>
 
 <script>
-    import AppButton from '@/components/core/elements/Button/AppButton';
+    import AppButton         from '@/components/core/elements/Button/AppButton';
+    import AppDropdownButton from '@/components/core/elements/Button/AppDropdownButton';
 
     export default {
         name: 'Buttons',
         components: {
-            AppButton
-        },
-        mounted() {
-            console.log(this);
+            AppButton,
+            AppDropdownButton
         }
     }
 </script>

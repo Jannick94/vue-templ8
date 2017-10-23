a1803d8b9072ff03b9e38e089061f6943c4812de
Jannick Berkhout <jannick@moodles.nl>
2017-10-23 23:38:41 +0200

    Fix mdi icons

--- src/main.js
+++ src/main.js
@@ -1,32 +1,25 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
+import AppButton       from './components/core/elements/Button/AppButton';
+import AppNotification from './components/core/elements/Notification/AppNotification';
+import AppIcon         from './components/core/elements/Icon/AppIcon';
+import AppInput        from './components/core/elements/Form/AppInput';
+import AppInputIcon    from './components/core/elements/Form/AppInputIcon';
+import AppRow          from './components/core/elements/Grid/AppRow';
+import AppCol          from './components/core/elements/Grid/AppCol';
+import AppLoader       from './components/core/elements/Loader/AppLoader';
 
-import AppButton       from '@/components/core/elements/Button/AppButton';
-import AppNotification from '@/components/core/elements/Notification/AppNotification';
-import AppIcon         from '@/components/core/elements/Icon/AppIcon';
-import AppInput        from '@/components/core/elements/Form/AppInput';
-import AppInputIcon    from '@/components/core/elements/Form/AppInputIcon';
-import AppRow          from '@/components/core/elements/Grid/AppRow';
-import AppCol          from '@/components/core/elements/Grid/AppCol';
-import AppLoader       from '@/components/core/elements/Loader/AppLoader';
+let Templ8 = {};
 
-Vue.component('AppButton', AppButton);
-Vue.component('AppNotification', AppNotification);
-Vue.component('AppIcon', AppIcon);
-Vue.component('AppInput', AppInput);
-Vue.component('AppInputIcon', AppInputIcon);
-Vue.component('AppRow', AppRow);
-Vue.component('AppCol', AppCol);
-Vue.component('AppLoader', AppLoader);
+Templ8.install = (Vue, options) => {
+    Vue.component('AppButton', AppButton);
+    Vue.component('AppNotification', AppNotification);
+    Vue.component('AppIcon', AppIcon);
+    Vue.component('AppInput', AppInput);
+    Vue.component('AppInputIcon', AppInputIcon);
+    Vue.component('AppRow', AppRow);
+    Vue.component('AppCol', AppCol);
+    Vue.component('AppLoader', AppLoader);
+}
 
-new Vue({
-    el: '#app',
-    router,
-    template: '<App/>',
-    components: {
-        App
-    }
-})
+export default Templ8;
